04682f479ccfbabed0722372c40dd59de84d0dcf
Antonino Natale <antonio.natale97@hotmail.com>
2021-11-17 22:28:14 +0100

    Fixed sources

--- src/main/java/it/unical/unijira/unijira/UniJiraApplication.java
+++ src/main/java/it/unical/unijira/unijira/UniJiraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UnijiraApplication {
+public class UniJiraApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UnijiraApplication.class, args);
+        SpringApplication.run(UniJiraApplication.class, args);
     }
 
 }
--- src/test/java/it/unical/unijira/unijira/UniJiraApplicationTests.java
+++ src/test/java/it/unical/unijira/unijira/UniJiraApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UnijiraApplicationTests {
+class UniJiraApplicationTests {
 
     @Test
     void contextLoads() {
