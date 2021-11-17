66d329e01c55a744bcbed0f07bfdc95b1a1566b2
Antonino Natale <antonio.natale97@hotmail.com>
2021-11-17 22:25:17 +0100

    First commit

---
+++ src/main/java/it/unical/unijira/unijira/UnijiraApplication.java
@@ -0,0 +1,13 @@
+package it.unical.unijira.unijira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnijiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnijiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unical/unijira/unijira/UnijiraApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.unijira.unijira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnijiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
