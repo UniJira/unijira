879aac5be1947a3036311148d021656a44df876d
Antonino Natale <antonio.natale97@hotmail.com>
2021-11-27 15:39:48 +0100

    Update Spring Configuration & CI

---
+++ src/main/java/it/unical/unijira/UniJiraConfig.java
@@ -0,0 +1,10 @@
+package it.unical.unijira;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class UniJiraConfig {
+
+}
