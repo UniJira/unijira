be2eaee17daec8876119469c8f585531bfdbf0a1
Antonino Natale <antonio.natale97@hotmail.com>
2021-12-19 20:05:56 +0100

    Update DTO

--- src/main/java/it/unical/unijira/data/dto/NotifyDTO.java
+++ src/main/java/it/unical/unijira/data/dto/NotifyDTO.java
@@ -1,18 +1,23 @@
 package it.unical.unijira.data.dto;
 
+import it.unical.unijira.data.models.Notify;
 import lombok.*;
 
 import java.net.URL;
+import java.time.LocalDateTime;
 
 
 @Builder
 @Getter @Setter @ToString
 @NoArgsConstructor @AllArgsConstructor
-public class NotifyDTO {
+public class NotifyDTO extends AbstractBaseDTO {
 
+    private Long id;
     private String title;
     private String message;
+    private Notify.Priority priority;
     private URL target;
     private Long userId;
+    private boolean read;
 
 }
