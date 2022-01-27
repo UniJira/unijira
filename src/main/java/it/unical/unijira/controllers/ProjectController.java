56e2277e63fbbe327d2ec7201db6cc2a4010e205
Giuseppe <peri.cle991@gmail.com>
2022-01-27 18:39:30 +0100

    Bugfix: solved this problem: [If a sprint is active, you can still change its project definition of done (solved in controllers!)]

--- src/main/java/it/unical/unijira/controllers/ProjectController.java
+++ src/main/java/it/unical/unijira/controllers/ProjectController.java
@@ -1448,6 +1448,14 @@ public class ProjectController implements CrudController<ProjectDTO, Long>  {
 
         definitionOfDoneEntryDTO.setProjectId(projectId);
 
+        Project project = projectService.findById(projectId).orElse(null);
+
+        if(project == null)
+            return ResponseEntity.badRequest().build();
+
+        if(sprintService.findActiveSprint(project).isPresent())
+            return ResponseEntity.badRequest().build();
+
         return definitionOfDoneEntryService.create(modelMapper.map(definitionOfDoneEntryDTO, DefinitionOfDoneEntry.class))
                 .map(found -> modelMapper.map(found, DefinitionOfDoneEntryDTO.class))
                 .map(ResponseEntity::ok)
@@ -1474,6 +1482,14 @@ public class ProjectController implements CrudController<ProjectDTO, Long>  {
         if(!definitionOfDoneEntryDTO.getProjectId().equals(projectId))
             return ResponseEntity.badRequest().build();
 
+        Project project = projectService.findById(projectId).orElse(null);
+
+        if(project == null)
+            return ResponseEntity.badRequest().build();
+
+        if(sprintService.findActiveSprint(project).isPresent())
+            return ResponseEntity.badRequest().build();
+
         return definitionOfDoneEntryService.update(entryId, modelMapper.map(definitionOfDoneEntryDTO, DefinitionOfDoneEntry.class))
                 .map(found -> modelMapper.map(found, DefinitionOfDoneEntryDTO.class))
                 .map(ResponseEntity::ok)
@@ -1484,6 +1500,15 @@ public class ProjectController implements CrudController<ProjectDTO, Long>  {
     @DeleteMapping("/{projectId}/defofdone/{entryId}")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Boolean> deleteDefinitionOfDoneEntry(@PathVariable Long projectId, @PathVariable Long entryId) {
+
+        Project project = projectService.findById(projectId).orElse(null);
+
+        if(project == null)
+            return ResponseEntity.badRequest().build();
+
+        if(sprintService.findActiveSprint(project).isPresent())
+            return ResponseEntity.badRequest().build();
+
         return definitionOfDoneEntryService.findById(entryId).stream()
                 .filter(found -> found.getProject().getId().equals(projectId))
                 .peek(definitionOfDoneEntryService::delete)
